0d0245c77662cf51d3c9ba93adf60f1b7735bbba
Sam-kayode <marvelloussamkayode@gmail.com>
2021-03-26 13:46:30 +0100

    firebase initialization

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
-    <Navigation/>
-    <router-view />
+    <Navigation :user="user" />
+    <router-view :user="user" />
   </div>
 </template>
 <script>
 import Navigation from '@/components/Navigation'
+import db from './db.js'
+
 export default {
-  components:{
-Navigation 
+  name: 'App',
+  data() {
+    return {
+      user: null
+    }
+  },
+  mounted() {
+    db.collection('users')
+      .doc('SbwYFOuT48yov90lcRKB')
+      .get()
+      .then(snapshot => {
+        this.user = snapshot.data().name
+      })
+  },
+  components: {
+    Navigation
   }
 }
 </script>
 
 <style lang="scss">
-$primary:#5f2882;
-@import 'node_modules/bootstrap/scss/bootstrap'
+$primary: #5f2882;
+@import 'node_modules/bootstrap/scss/bootstrap';
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-   <h1>Home</h1>
+    <h1>Home</h1>
+    <p>welcome {{ user }}</p>
   </div>
 </template>
 
 <script>
 export default {
-
-};
+  name: 'Home'
+}
 </script>
