e996c228db22db11985df8ca31e790f0bbbb416a
Sam-kayode <marvelloussamkayode@gmail.com>
2021-03-27 02:20:14 +0100

    nav bar added

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-      <router-link to="/">Home</router-link>| 
-      <router-link to="/login">Login</router-link>
-  </div>
+ <div>
+  <nav class="navbar navbar-expand bg-primary navbar-dark">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/"
+        ><span class="h2 pr-1">Video Chat</span>
+        <span class="navbar-text small">
+          : Hi <span class="font-weight-bold text-white">{{ user }}</span></span
+        >
+      </router-link>
+      <div class="navbar-nav ml-auto">
+        <router-link class="nav-item nav-link" to="/login">log in</router-link>
+      </div>
+    </div>
+  </nav>
+</div>
 </template>
 
