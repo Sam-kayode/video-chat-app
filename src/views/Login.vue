2375fb9e95ae0f9c5813f69ab20ea69667b01de3
Sam-kayode <marvelloussamkayode@gmail.com>
2021-04-03 14:39:42 +0100

    login page with firebase auth

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login row align-items-center justify-content-center p-0 mx-0">
+  <div class="login row container-fluid align-items-center justify-content-center p-0 mx-0">
     <div class="col-sm col-md-6 p-0">
       <img
         src="https://res.cloudinary.com/sam-kay/image/upload/q_auto:low/v%20chat/20945760-min_1_1_xweu9y.png"
@@ -8,11 +8,12 @@
       />
     </div>
     <div class="col-sm col-md-6 row mx-0">
-      <form class="card bg-light col-md-11 col-lg-7 container p-5">
+      <form @submit.prevent="login" class="card bg-light col-md-11 col-lg-7 container p-3 py-5">
         <h3 class="font-weight-bold text-primary login-heading mb-4">
           Hello, <br />
           Welcome back!
         </h3>
+        <div class="alert alert-danger px-3" v-if="error">{{ error }}</div>
         <div class="form-group">
           <label for="exampleInputEmail1">Email address</label>
           <input
@@ -21,6 +22,7 @@
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             placeholder="Enter email"
+            v-model="email"
           />
           <small id="emailHelp" class="form-text text-muted"
             >We'll never share your email with anyone else.</small
@@ -33,20 +35,59 @@
             class="form-control"
             id="exampleInputPassword1"
             placeholder="Password"
+            v-model="password"
           />
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
+        <Loader v-if="loader" />
       </form>
     </div>
   </div>
 </template>
 <script>
-export default {}
-</script>
-<style lang="scss">
-.login{
-height:90vh;
-overflow-y: hidden;
+import Firebase from 'firebase'
+import Loader from '../components/loader.vue'
+export default {
+  data() {
+    return {
+      email: null,
+      password: null,
+      error: null,
+      loader: false
+    }
+  },
+  components: { Loader },
+  methods: {
+    login() {
+      if (this.email && this.password) {
+        this.loader = true
+      }
+      const info = {
+        email: this.email,
+        password: this.password
+      }
+      Firebase.auth()
+        .signInWithEmailAndPassword(info.email, info.password)
+        .then(
+          () => {
+            this.$router.push('/')
+            this.loader = false
+          },
+          error => {
+            this.error = error.message
+            this.loader = false
+          }
+        )
+    }
+  }
 }
+</script>
 
+<style lang="scss">
+.login {
+  height: 94vh;
+}
+.login-img {
+  width: 95%;
+}
 </style>
