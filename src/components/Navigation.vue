efbe978730d2e0a7c710c21bdd5914644710505c
Sam-kayode <marvelloussamkayode@gmail.com>
2021-05-14 18:50:08 +0100

    harmburger styling

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -8,25 +8,24 @@
           <router-link class="px-2" to="/login" v-if="!user">log in</router-link>
           <span class="px-2" @click="$emit('signout')" v-if="user">Sign out</span>
         </div>
-          <button
-        class="harmburger"
-        :class="[{ opened: toggled }]"
-        @click="toggle"
-        aria-label="Main Menu"
-      >
-        <svg width="100" height="100" viewBox="0 0 100 100">
-          <path
-            class="line line1"
-            d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
-          />
-          <path class="line line2" d="M 20,50 H 80" />
-          <path
-            class="line line3"
-            d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
-          />
-        </svg>
-      </button>
-
+        <button
+          class="harmburger"
+          :class="[{ opened: toggled }]"
+          @click="toggle"
+          aria-label="Main Menu"
+        >
+          <svg width="100" height="100" viewBox="0 0 100 100">
+            <path
+              class="line line1"
+              d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+            />
+            <path class="line line2" d="M 20,50 H 80" />
+            <path
+              class="line line3"
+              d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+            />
+          </svg>
+        </button>
 
         <div class="navbar-nav ml-auto">
           <router-link class="navbar-brand p-0" to="/"
@@ -40,29 +39,30 @@
         </div>
       </div>
     </nav>
-    <div class="sidenav">
-       <ul class="p-0">
-         <li> <router-link class="px-2 py-4" to="/rooms" v-if="user">Rooms</router-link></li>
-       <router-link class="px-2 py-4" to="/register">Register</router-link>
-         <li>  <span class="px-2 py-4" @click="$emit('signout')" v-if="user">Sign out</span></li>
-       </ul>
-    </div>
+    <transition name="fade">
+      <div class="sidenav" v-if="toggled">
+        <ul class="p-0">
+          <li><router-link class="px-2 py-4" to="/rooms" v-if="user">Rooms</router-link></li>
+          <router-link class="px-2 py-4" to="/register">Register</router-link>
+          <li><span class="px-2 py-4" @click="$emit('signout')" v-if="user">Sign out</span></li>
+        </ul>
+      </div>
+    </transition>
   </div>
 </template>
-
 
 <script>
 export default {
   name: '',
   props: ['user'],
   data() {
-    return { toggled: false };
+    return { toggled: false }
   },
-   methods: {
+  methods: {
     toggle() {
-      this.toggled = !this.toggled;
-    },
-  },
+      this.toggled = !this.toggled
+    }
+  }
 }
 </script>
 
@@ -83,28 +83,25 @@ nav {
   font-size: 20px;
 }
 
-.sidenav{
-  width:300px;
-  height:100vh;
-  position:fixed;
-  z-index:10;
+.sidenav {
+  width: 300px;
+  height: 100vh;
+  position: fixed;
+  z-index: 10;
   background-color: rgb(244, 246, 248);
 }
 
-.sidenav ul{
-   display: flex;
+.sidenav ul {
+  display: flex;
   justify-content: center;
   align-items: center;
-  height:100%;
+  height: 100%;
   flex-direction: column;
-  
-  
 }
 
-.sidenav ul li{
- list-style-type: none;
+.sidenav ul li {
+  list-style-type: none;
 }
-
 
 .harmburger {
   background-color: #ffffff;
@@ -159,19 +156,24 @@ svg {
   width: 40px;
 }
 
-@media(min-width:576px) {
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 
-  .sidenav, .harmburger{
+@media (min-width: 576px) {
+  .sidenav,
+  .harmburger {
     display: none;
-    
   }
 }
 
-@media(max-width:576px) {
-  
-  .topnav-links{
+@media (max-width: 576px) {
+  .topnav-links {
     display: none;
-    
   }
 }
 </style>
