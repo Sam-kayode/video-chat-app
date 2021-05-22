e30cdaa9d9fe425b34c0e3f743679c727866a54f
Sam-kayode <marvelloussamkayode@gmail.com>
2021-05-22 20:07:20 +0100

    rooms list stacking

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -27,41 +27,28 @@
               placeholder="Password"
             />
           </div>
-        
-      
+
           <button type="submit" class="btn btn-primary">Add room</button>
         </form>
       </div>
       <div class="col-sm col-md-7">
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-            <small id="emailHelp" class="form-text text-muted"
-              >We'll never share your email with anyone else.</small
-            >
+        <div class="row">
+          <div class="col-sm-8">
+            <div class="card">
+              <h5 class="card-header">Featured</h5>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">
+                  With supporting text below as a natural lead-in to additional content.
+                </p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-            />
+           <div class="col-sm-4">
+           
           </div>
-          <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
-          <button type="submit" class="btn btn-primary">Add room</button>
-        </form>
+        </div>
       </div>
     </div>
   </div>
