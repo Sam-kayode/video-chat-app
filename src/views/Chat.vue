be4f6bc743dd50c6374277acac634b05dc5e214b
Sam-kayode <marvelloussamkayode@gmail.com>
2021-07-29 22:58:56 +0100

    approving attendees

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -2,23 +2,31 @@
   <div class="container-fluid mt-4">
     <div class="mb-3">
       <span class="mb-0 h2 text-primary">{{ roomName }}</span>
-      <span class="ml-1">
+      <span class="ml-1" v-if="user">
         Hosted by: <strong class="text-danger">{{ hostDisplayName }}</strong>
       </span>
     </div>
-    <div class="row" v-if="user !==null && user.uid == hostID">
+    <div class="row" v-if="(user !== null && user.uid == hostID) || attendeeApproved">
       <div class="col-md-8"></div>
       <div class="col-md-4">
         <button class="btn btn-primary mr-1">Join</button>
         <button type="button" class="btn btn-danger mr-1">Leave</button>
         <h4 class="mt-2">Attendees</h4>
         <ul class="list-unstyled">
-          <li>
+          <li v-for="attendee in attendeesApproved" :key="attendee.id">
+            <a
+              type="button"
+              class="mr-2"
+              title="Approve attendee"
+              @click="toggleApproval(attendee.id)"
+            >
+              <font-awesome-icon icon="user"></font-awesome-icon>
+            </a>
             <span class="mr-2" title="On Air">
               <font-awesome-icon icon="podcast"></font-awesome-icon>
             </span>
             <span></span>
-            <span class="pl-1"></span>
+            <span class="pl-1">{{ attendee.displayName }}</span>
           </li>
         </ul>
         <div>
@@ -26,14 +34,24 @@
           <ul class="list-unstyled">
             <li class="mb-1" v-for="attendee in attendeesPending" :key="attendee.id">
               <span>
-                <a type="button" class="mr-2" title="Approve attendee">
+                <a
+                  type="button"
+                  class="mr-2"
+                  title="Approve attendee"
+                  @click="toggleApproval(attendee.id)"
+                >
                   <font-awesome-icon icon="user"></font-awesome-icon>
                 </a>
-                <a type="button" class="text-secondary pr-1" title="Delete Attendee">
+                <a
+                  type="button"
+                  class="text-secondary pr-1"
+                  title="Delete Attendee"
+                  @click="deleteAttendee(attendee.id)"
+                >
                   <font-awesome-icon icon="trash"></font-awesome-icon>
                 </a>
               </span>
-              <span class="pl-1">{{attendee.displayName}}</span>
+              <span class="pl-1">{{ attendee.displayName }}</span>
             </li>
           </ul>
         </div>
@@ -54,7 +72,9 @@ export default {
   name: 'Attendees',
   data() {
     return {
-      attendeesPending:[],
+      attendeesApproved: [],
+      attendeesPending: [],
+      attendeeApproved: false,
       hostID: this.$route.params.hostID,
       roomID: this.$route.params.roomID,
       roomName: null,
@@ -63,6 +83,44 @@ export default {
   },
   components: {
     FontAwesomeIcon
+  },
+  methods: {
+    toggleApproval(attendeeID) {
+      if (this.user && this.user.uid == this.hostID) {
+        const ref = db
+          .collection('users')
+          .doc(this.user.uid)
+          .collection('rooms')
+          .doc(this.roomID)
+          .collection('attendees')
+          .doc(attendeeID)
+
+          ref.get().then(attendeeDocument =>{
+            const approved = attendeeDocument.data().approved
+            if(approved){
+              ref.update({
+                approved:!approved
+              })
+            }else{
+              ref.update({
+                approved:true
+              })
+            }
+
+          })
+      }
+    },
+    deleteAttendee(attendeeID) {
+      if (this.user && this.user.uid == this.hostID) {
+        db.collection('users')
+          .doc(this.user.uid)
+          .collection('rooms')
+          .doc(this.roomID)
+          .collection('attendees')
+          .doc(attendeeID)
+          .delete()
+      }
+    }
   },
   props: ['user'],
   mounted() {
@@ -78,6 +136,7 @@ export default {
     })
     roomRef.collection('attendees').onSnapshot(attendeeSnapshot => {
       const tempPending = []
+      const tempApproved = []
       let amCheckedIn = false
       attendeeSnapshot.forEach(attendeeDocument => {
         if (this.user.uid == attendeeDocument.id) {
@@ -86,12 +145,29 @@ export default {
         if (this.hostID == attendeeDocument.id) {
           this.hostDisplayName = attendeeDocument.data().displayName
         }
-        tempPending.push({
-          id:attendeeDocument.id,
-          displayName: attendeeDocument.data().displayName
-        })
+        if (attendeeDocument.data().approved) {
+          if (this.user.uid == attendeeDocument.id) {
+            this.attendeeApproved = true
+          }
+          tempApproved.push({
+            id: attendeeDocument.id,
+            displayName: attendeeDocument.data().displayName,
+            approved: attendeeDocument.data().approved
+          })
+        } else {
+          if (this.user.uid == attendeeDocument.id) {
+            this.attendeeApproved = false
+          }
+
+          tempPending.push({
+            id: attendeeDocument.id,
+            displayName: attendeeDocument.data().displayName,
+            approved: attendeeDocument.data().approved
+          })
+        }
       })
-      this.attendeesPending =tempPending
+      this.attendeesApproved = tempApproved
+      this.attendeesPending = tempPending
       if (!amCheckedIn) {
         this.$router.push('/checkin/$(this.hostID}/${this.roomID}')
       }
@@ -100,5 +176,4 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style></style>
